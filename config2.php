4fc92b17d2ae236c67e64428c6d1a11eadcae2cf
Tad <tad0616@gmail.com>
2015-07-02 16:32:30 +0800

    加入滑動圖文功能

--- config2.php
+++ config2.php
@@ -1,9 +1,7 @@
 <?php
-$i=0;
-$theme_config[$i]['name']="show_var";
-$theme_config[$i]['text']=TF_SHOW_VAR;
-$theme_config[$i]['desc']=TF_SHOW_VAR_DESC;
-$theme_config[$i]['type']="yesno";
-$theme_config[$i]['default']="0";
-
-?>
+$i                           = 0;
+$theme_config[$i]['name']    = "show_var";
+$theme_config[$i]['text']    = TF_SHOW_VAR;
+$theme_config[$i]['desc']    = TF_SHOW_VAR_DESC;
+$theme_config[$i]['type']    = "yesno";
+$theme_config[$i]['default'] = "0";
